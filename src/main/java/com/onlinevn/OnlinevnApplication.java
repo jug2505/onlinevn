e00d53a58a6ab1d0aba3018ea1ad412c11e3a97d
jug2505 <jonkuvshin@gmail.com>
2021-10-15 13:44:13 +0300

    Initial commit

---
+++ src/main/java/com/onlinevn/OnlinevnApplication.java
@@ -0,0 +1,13 @@
+package com.onlinevn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinevnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinevnApplication.class, args);
+	}
+
+}
